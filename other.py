7019090fa01a78eb3bda3d32c29958cca73f4dbc
Igor <nelubov.igor@gmail.com>
2025-02-04 18:53:00 +0300

    other update

--- other.py
+++ other.py
@@ -1,6 +1,16 @@
 from array import array
+from contextlib import contextmanager
+
+@contextmanager
+def ctx():
+    print(1)
+    try: yield
+    finally: print(2)
+
+with ctx(), ctx():
+    print(3)
 
 a = array('i', [1, 2, 3])
 b = memoryview(a).cast("B")
 a[0] = 256
-print(b[0], b[1])
+# print(b[0], b[1])
