a47e38da89a9c9167ad5769b7148db23a5309bea
Igor <nelubov.igor@gmail.com>
2025-02-11 15:36:11 +0300

    hi2

--- other.py
+++ other.py
@@ -1,27 +1,29 @@
 from array import array
 from contextlib import contextmanager
 
-class Meta(type):
-    def __call__(self, *args, **kwds):
-        print(1)
-        return super().__call__(*args, **kwds)
+# class Meta(type):
+#     def __call__(self, *args, **kwds):
+#         print(1)
+#         return super().__call__(*args, **kwds)
 
-class A(metaclass=Meta):
-    def __init__(self):
-        print(2)
-    def __new__(cls):
-        print(3)
-        return super().__new__(cls)
+# class A(metaclass=Meta):
+#     def __init__(self):
+#         print(2)
+#     def __new__(cls):
+#         print(3)
+#         return super().__new__(cls)
 
-A()
-@contextmanager
-def ctx():
-    print(1)
-    try: yield
-    finally: print(2)
+# A()
 
-with ctx(), ctx():
-    print(3)
+
+# @contextmanager
+# def ctx():
+#     print(1)
+#     try: yield
+#     finally: print(2)
+
+# with ctx(), ctx():
+#     print(3)
 
 a = array('i', [1, 2, 3])
 b = memoryview(a).cast("B")
